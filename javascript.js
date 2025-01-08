0329c465ebaba2e9bf186aa070565cddef69f3c0
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-08 16:47:18 +0800

    feat: add playRound function in game board module

--- javascript.js
+++ javascript.js
@@ -1,16 +1,23 @@
 // Gameboard Module
 const gameBoard = (function(){
-    const gameBoardArr = ["", "", "", "", "", "", "", "", ""];
-    const consoleBoard = `    ${gameBoardArr[0]} | ${gameBoardArr[1]} | ${gameBoardArr[2]}
+    let gameBoardArr = ["", "", "", "", "", "", "", "", ""];
+    let consoleBoard = function() {
+        return `    ${gameBoardArr[0]} | ${gameBoardArr[1]} | ${gameBoardArr[2]}
     - - - - - 
     ${gameBoardArr[3]} | ${gameBoardArr[4]} | ${gameBoardArr[5]}
     - - - - - 
-    ${gameBoardArr[6]} | ${gameBoardArr[7]} | ${gameBoardArr[8]}`;
+    ${gameBoardArr[6]} | ${gameBoardArr[7]} | ${gameBoardArr[8]}`};
 
-    return { gameBoardArr, consoleBoard };
+    const playRound = function(position, marker) {
+        gameBoardArr[position] = marker;
+        consoleBoard();
+        console.log(consoleBoard());
+    };
+
+    return { gameBoardArr, consoleBoard, playRound };
 })();
 
-console.log(gameBoard.consoleBoard);
+console.log(gameBoard.consoleBoard());
 
 // Players Factory
 const players = function(playerOne = "Player One", playerTwo = "Player Two") {
@@ -22,6 +29,12 @@ const players = function(playerOne = "Player One", playerTwo = "Player Two") {
     };
 };
 
-console.log(players());
-
-// gameBoard.gameBoardArr[0] = players()
+gameBoard.playRound(0, players()["Player 1"]["marker"]);
+gameBoard.playRound(1, players()["Player 1"]["marker"]);
+gameBoard.playRound(2, players()["Player 1"]["marker"]);
+gameBoard.playRound(3, players()["Player 1"]["marker"]);
+gameBoard.playRound(4, players()["Player 1"]["marker"]);
+gameBoard.playRound(5, players()["Player 1"]["marker"]);
+gameBoard.playRound(6, players()["Player 1"]["marker"]);
+gameBoard.playRound(7, players()["Player 1"]["marker"]);
+gameBoard.playRound(8, players()["Player 1"]["marker"]);
