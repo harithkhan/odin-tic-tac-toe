08c9e3ce85874e30d2e093168956f41c761dd969
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-09 09:36:04 +0800

    feat: add logic to end game with win conditions Add win function factory that returns an object containing win conditions. Add conditional statement that assign gameOver variable to true if win conditions are met, and prints the winner of the game.

--- javascript.js
+++ javascript.js
@@ -32,9 +32,34 @@ const TTTPlayers = function(playerOne = "Player One", playerTwo = "Player Two") 
 
 // Start Game Factory
 const startTTTGame = function() {
+
+    // Win Logic
+    const win = function () {
+        return {
+            1: (gameBoard.gameBoardArr[0] === "X" && gameBoard.gameBoardArr[1] === "X" && gameBoard.gameBoardArr[2] === "X"),
+            2: (gameBoard.gameBoardArr[3] === "X" && gameBoard.gameBoardArr[4] === "X" && gameBoard.gameBoardArr[5] === "X"),
+            3: (gameBoard.gameBoardArr[6] === "X" && gameBoard.gameBoardArr[7] === "X" && gameBoard.gameBoardArr[8] === "X"),
+            4: (gameBoard.gameBoardArr[0] === "X" && gameBoard.gameBoardArr[3] === "X" && gameBoard.gameBoardArr[6] === "X"),
+            5: (gameBoard.gameBoardArr[1] === "X" && gameBoard.gameBoardArr[4] === "X" && gameBoard.gameBoardArr[7] === "X"),
+            6: (gameBoard.gameBoardArr[2] === "X" && gameBoard.gameBoardArr[5] === "X" && gameBoard.gameBoardArr[8] === "X"),
+            7: (gameBoard.gameBoardArr[0] === "X" && gameBoard.gameBoardArr[4] === "X" && gameBoard.gameBoardArr[8] === "X"),
+            8: (gameBoard.gameBoardArr[2] === "X" && gameBoard.gameBoardArr[4] === "X" && gameBoard.gameBoardArr[6] === "X"),
+            9: (gameBoard.gameBoardArr[0] === "X" && gameBoard.gameBoardArr[1] === "X" && gameBoard.gameBoardArr[2] === "X"),
+            10: (gameBoard.gameBoardArr[3] === "O" && gameBoard.gameBoardArr[4] === "O" && gameBoard.gameBoardArr[5] === "O"),
+            11: (gameBoard.gameBoardArr[6] === "O" && gameBoard.gameBoardArr[7] === "O" && gameBoard.gameBoardArr[8] === "O"),
+            12: (gameBoard.gameBoardArr[0] === "O" && gameBoard.gameBoardArr[3] === "O" && gameBoard.gameBoardArr[6] === "O"),
+            13: (gameBoard.gameBoardArr[1] === "O" && gameBoard.gameBoardArr[4] === "O" && gameBoard.gameBoardArr[7] === "O"),
+            14: (gameBoard.gameBoardArr[2] === "O" && gameBoard.gameBoardArr[5] === "O" && gameBoard.gameBoardArr[8] === "O"),
+            15: (gameBoard.gameBoardArr[0] === "O" && gameBoard.gameBoardArr[4] === "O" && gameBoard.gameBoardArr[8] === "O"),
+            16: (gameBoard.gameBoardArr[2] === "O" && gameBoard.gameBoardArr[4] === "O" && gameBoard.gameBoardArr[6] === "O")
+        };
+    };
+
+    // Initialize turn and gameOver status
     let turn = "playerOne";
     let gameOver = false;
-    console.log("Game Starts!")
+    console.log("Game Starts!");
+
     function playTurn() {
         if (turn === "playerOne") {
             console.log("Player 1's turn.");
@@ -42,6 +67,13 @@ const startTTTGame = function() {
             const playerMark = prompt("Player 1: please enter board number where you would like to place your turn");
             gameBoard.markBoard(playerMark, TTTPlayerOne["Player 1"]["marker"]);
             console.log(`Player 1 marked box ${playerMark}.`);
+            for (let winCondition of Object.values(win())) {
+                if (winCondition) {
+                    gameOver = true;
+                    console.log(gameBoard.consoleBoard());
+                    console.log("Player 1 has won!");
+                };
+            };
             turn = "playerTwo";
         } else if (turn === "playerTwo") {
             console.log("Player 2's turn.");
@@ -49,12 +81,19 @@ const startTTTGame = function() {
             const playerMark = prompt("Player 2: please enter board number where you would like to place your turn");
             gameBoard.markBoard(playerMark, TTTPlayerTwo["Player 2"]["marker"]);
             console.log(`Player 2 marked box ${playerMark}.`);
+            for (let winCondition of Object.values(win())) {
+                if (winCondition) {
+                    gameOver = true;
+                    console.log(gameBoard.consoleBoard());
+                    console.log("Player 2 has won!");
+                };
+            };
             turn = "playerOne";
         };
     };
-    playTurn();
-    playTurn();
-    playTurn();
+    while (!gameOver) {
+        playTurn();
+    };
 };
 
-startTTTGame();
+startTTTGame(); // Initialize game
